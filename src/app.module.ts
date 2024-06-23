a91d373ea2019c93315c6c2ada459ebc9dbf0bca
Seongwon Kim <seongwon.kim@modelical.com>
2024-06-23 12:32:39 +0900

    - add azure bolbs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AzureBlobService } from './azure-blob/azure-blob.service';
+import { AzureBlobController } from './azure-blob/azure-blob.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AzureBlobController],
+  providers: [AppService, AzureBlobService],
 })
 export class AppModule {}
