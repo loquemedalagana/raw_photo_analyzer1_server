23c177596485ad9b23c4bed35cb291522755a079
Seongwon Kim <seongwon.kim@modelical.com>
2024-06-23 19:43:32 +0900

    - create file download method but is not working

--- src/azure-blob/azure-blob.controller.ts
+++ src/azure-blob/azure-blob.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AzureBlobService } from './azure-blob.service';
 
 @Controller('blobs')
@@ -8,5 +9,15 @@ export class AzureBlobController {
   async listFiles() {
     const blobNames = await this.azureBlobService.listBlobs();
     return { blobNames };
+  }
+
+  @Get('download/:fileName')
+  async downloadFile(
+    @Param('fileName') fileName: string,
+    @Res() res: Response,
+  ) {
+    const fileBuffer = await this.azureBlobService.downloadBlob(fileName);
+    res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
+    res.send(fileBuffer);
   }
 }
